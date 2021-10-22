65dcf00b1bbad669b945669320756e9f8dfb2cd8
xaedes <xaedes@gmail.com>
2021-10-22 21:47:10 +0200

    fix comment

--- gl_classes/find_packages.cmake
+++ gl_classes/find_packages.cmake
@@ -3,7 +3,7 @@ cmake_minimum_required(VERSION 3.8)
 #                             # imported target
 #-----------------------------#----------------
 find_package(OpenGL REQUIRED) # OpenGL::GL
-find_package(glm REQUIRED)    # glm
+find_package(glm REQUIRED)    # glm::glm
 find_package(glfw3 REQUIRED)  # glfw
 find_package(glew REQUIRED)   # GLEW::glew
 find_package(imgui REQUIRED)  # imgui::imgui
