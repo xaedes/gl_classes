008b232add73891bf28622bf6b8a7be8470efac6
xaedes <xaedes@gmail.com>
2022-06-23 18:21:17 +0200

    only do find_package(imgui) if target imgui does not exist

--- gl_classes/find_packages.cmake
+++ gl_classes/find_packages.cmake
@@ -6,4 +6,6 @@ find_package(OpenGL REQUIRED) # OpenGL::GL
 find_package(glm REQUIRED)    # glm::glm
 find_package(glfw3 REQUIRED)  # glfw
 find_package(glew REQUIRED)   # GLEW::glew
-find_package(imgui REQUIRED)  # imgui::imgui
+if (NOT TARGET imgui AND NOT TARGET imgui::imgui)
+    find_package(imgui REQUIRED)  # imgui::imgui
+endif()
